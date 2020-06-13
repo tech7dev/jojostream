33f3e414ea8d6b798cf9f7e7d84c8b1beac5d913
tech7dev <tech7dev@gmail.com>
2020-06-13 15:25:59 +0200

    Initial Commit

---
+++ app/src/main/java/com/tech7/jojostream/Provider/GenericFileProvider.java
@@ -0,0 +1,8 @@
+package com.tech7.jojostream.Provider;
+
+
+import androidx.core.content.FileProvider;
+
+public class GenericFileProvider extends FileProvider {
+
+}
---
+++ app/src/main/java/com/tech7/jojostream/event/CastSessionStartedEvent.java
@@ -0,0 +1,13 @@
+package com.tech7.jojostream.event;
+
+/**
+ * Created by Tamim on 28/09/2019.
+
+ */
+
+public class CastSessionStartedEvent {
+
+    public CastSessionStartedEvent() {
+
+    }
+}
